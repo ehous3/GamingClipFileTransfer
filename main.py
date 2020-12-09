6188bfb5dd981371723d186516367a6c8ff37783
Ethan Houseworth <ejhouseworthh@gmail.com>
2020-12-08 22:42:39 -0600

    Add files via upload
    Main file

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import os.path
+from pydrive.drive import GoogleDrive 
+from pydrive.auth import GoogleAuth 
+from datetime import datetime
+
+#get newest file
+path = r'C:\Users\ethan\Videos\Captures'
+filelist = [os.path.join(path, f) for f in os.listdir(path)]
+filelist = [f for f in filelist if os.path.isfile(f)]
+newest = max(filelist, key=lambda x: os.stat(x).st_mtime)
+print(newest)
+
+#get the date and time plus the .mp4 extension
+todaysDate = newest[-23:]
+
+#gauth process
+gauth = GoogleAuth() 
+gauth.LocalWebserverAuth()        
+drive = GoogleDrive(gauth) 
+
+#varible defining
+title = "Newest Gaming Clip" + todaysDate
+fid = "1VKHKRpli4tY6JwMawlQ_4puXeNQ6YyIQ"
+filename = newest[-60:]
+
+#upload clip
+file = drive.CreateFile({'title': title,
+                         "parents": [{"kind": "drive#fileLink", "id": fid}]})
+file.SetContentFile(path + "\\" + filename)
+file.Upload()                         
+
